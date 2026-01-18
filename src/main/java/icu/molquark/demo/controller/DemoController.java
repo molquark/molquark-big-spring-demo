530de8251a19ea9881c246dbd3e472e16171b318
molquark <molquark@qq.com>
2026-01-18 15:42:48 +0800

    project demo

---
+++ src/main/java/icu/molquark/demo/controller/DemoController.java
@@ -0,0 +1,19 @@
+package icu.molquark.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/test")
+public class DemoController {
+
+    @GetMapping("/greeting")
+    public String greeting(){
+        return "Hello World";
+    }
+
+
+
+
+}
